1372e182b32c399b2075574b99e84a88231494fb
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-07-23 15:30:01 +0300

    "feat: создать контроллеры для пользователей"

--- controllers/users.js
+++ controllers/users.js
@@ -5,3 +5,24 @@ module.exports.getUsers = (req, res) => {
     .then(users => res.send({ data: users }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
+
+module.exports.getCurrentUser = (req, res) => {
+  User.findById(req.user._id).select('+password')
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch(() => res.send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.createUser = (req, res) => {
+  const {
+    email, password, name, about, avatar,
+  } = req.body;
+
+  User.findOne({ email })
+    .then((hash) => User.create({
+      email, password: hash, name, about, avatar,
+    }))
+    .then((user) => res.send({ _id: user._id, email: user.email }))
+    .catch(() => res.send({ message: 'Произошла ошибка' }));
+};

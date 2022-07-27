2cb929e66b8147eb6cec084eb269261465aec35d
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-07-27 09:15:38 +0300

    "ref: еще улучшить код"

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cards = require('./routes/cards');
 const users = require('./routes/users');
 const pathNotFound = require('./routes/not-found');
 
-const ERROR_SERVER = 500;
-
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -26,18 +24,10 @@ app.use(cards);
 app.use(users);
 app.use(pathNotFound);
 
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
-
-// app.use((err, req, res, next) => {
-//   const { statusCode = ERROR_SERVER, message } = err;
-//   const errorMessage = (statusCode === ERROR_SERVER) ? 'Ошибка на сервере' : message;
-//   res.status(statusCode).send({ message: errorMessage });
-//   next();
-// });

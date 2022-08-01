691b8dc0b57021e7c773daed60d4c906e8fc5ce4
Ekaterina Topchieva <ekaterinatop4ieva@yandex.ru>
2022-08-01 22:02:54 +0300

    "fix: поправить next"

--- controllers/users.js
+++ controllers/users.js
@@ -93,7 +93,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
     })
-    .catch(next(new UnauthorizedError('Неправильные почта или пароль')));
+    .catch(() => next(new UnauthorizedError('Неправильные почта или пароль')));
 };
 
 module.exports.getCurrentUser = (req, res, next) => {
